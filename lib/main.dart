4b81b9bf51113108304a90daa4e4667769296d2e
Surya <surya.saravanakumar@sharpsell.ai>
2024-01-03 10:25:17 +0530

    fix: final worlomg

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:just_audio/just_audio.dart';
+import 'package:path/path.dart' as p;
+// import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 import 'dart:io';
-import 'package:audioplayers/audioplayers.dart';
+// import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
+// import 'package:just_audio/just_audio.dart';
 import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+
+import 'package:universal_platform/universal_platform.dart';
 
 void main() {
   runApp(const RecordingScreen());
@@ -19,16 +29,19 @@ class RecordingScreen extends StatefulWidget {
 }
 
 class _RecordingScreenState extends State<RecordingScreen> {
+  // late AudioRecorder audioRecord;
   late Record audioRecord;
-  late AudioPlayer audioPlayer;
+  // late AudioPlayer audioPlayer;
+  final audioPlayer = AudioPlayer();
   bool isRecording = false;
   String audioPath = "";
 
   @override
   void initState() {
     super.initState();
-    audioPlayer = AudioPlayer();
+    // audioPlayer = AudioPlayer();
     audioRecord = Record();
+    // audioRecord = AudioRecorder();
   }
 
   @override
@@ -40,10 +53,31 @@ class _RecordingScreenState extends State<RecordingScreen> {
   bool playing=false;
   Future<void> startRecording() async {
     try {
-      print("START RECODING+++++++++++++++++++++++++++++++++++++++++++++++++");
-      if (await audioRecord.hasPermission()) {
+      // Surya here is the issue on mobile
+      String directoryPath = '';
+      if (!UniversalPlatform.isWeb){
+        final Directory extDir = await getApplicationDocumentsDirectory();
+        directoryPath = '${extDir.path}/smart_sell_pdf';
+        await Directory(directoryPath).create(recursive: true);
+      }
 
-        await audioRecord.start(path: '${audioPath}/myFile.m4a');
+
+      if (await audioRecord.hasPermission()) {
+       print("Surya record has permission");
+       AudioEncoder encoder = UniversalPlatform.isIOS ? AudioEncoder.pcm16bit : AudioEncoder.aacLc;
+        // await audioRecord.start(path: '$directoryPath/myFile.mp3',encoder:encoder);
+        // final RecordConfig config = RecordConfig(encoder: encoder);
+        // in iOS file should be m4a
+       // todo: check conversion for m4a to mp3
+       String filePath = '';
+       if (UniversalPlatform.isWeb){
+         filePath = '${audioPath}/${DateTime.now()}-myFile.mp3}';
+       } else {
+         filePath = '$directoryPath/${DateTime.now()}-myFile.${UniversalPlatform.isIOS ? 'm4a': 'mp3'}';
+       }
+       print("START RECODING+++++++++++++ $filePath ++++++++++++++++++++++++++++++++++++");
+        // await audioRecord.start(config, path: filePath);//m4a//mp3
+        await audioRecord.start(path: filePath,encoder: encoder);
         setState(() {
           isRecording = true;
         });
@@ -55,8 +89,8 @@ class _RecordingScreenState extends State<RecordingScreen> {
 
   Future<void> stopRecording() async {
     try {
-      print("STOP RECODING+++++++++++++++++++++++++++++++++++++++++++++++++");
       String? path = await audioRecord.stop();
+      print("STOP RECODING+++++++++++++++${path}++++++++++++++++++++++++++++++++++");
       setState(() {
         recoding_now=false;
         isRecording = false;
@@ -72,22 +106,62 @@ class _RecordingScreenState extends State<RecordingScreen> {
       playing = true;
       setState(() {});
 
-      print("AUDIO PLAYING+++++++++++++++++++++++++++++++++++++++++++++++++");
-      Source urlSource = UrlSource(audioPath);
-      await audioPlayer.play(urlSource);
+
+      print("AUDIO PLAYING+++++++++++++++++++ $audioPath ++++++++++++++++++++++++++++++");
+      // if (UniversalPlatform.isWeb) {
+      //   print("audio play here");
+      //   final Uint8List bytes = await File('${audioPath}/myFile.mp3').readAsBytes();
+      //   // List<int> list = utf8.encode('${audioPath}/myFile.mp3');
+      //   // Uint8List bytes = Uint8List.fromList(list);
+      //   print("audio play here 1");
+      //   // await audioPlayer.setFilePath('${audioPath}/myFile.m4a'); // Replace with your file path
+      //   await audioPlayer.setAudioSource(WebBufferAudioSource(bytes));
+      //   print("audio play here 2");
+      // } else {
+      //   await audioPlayer.setFilePath('${audioPath}/myFile.m4a');
+      // }
+      // print("audio play here 3");
+      // audioPlayer.play();
+      print("audio play here");
+      print('Surya - audio path - $audioPath');
+// if (Platform.isIOS){
+//   DeviceFileSource urlSource = DeviceFileSource(audioPath);
+//   print("audio play here 1 url source- $urlSource");
+//   await audioPlayer.play(urlSource);
+// } else {
+//   Source urlSource = UrlSource(audioPath);
+//   print("audio play here 1 url source- $urlSource");
+//   await audioPlayer.play(urlSource);
+// }
+
+
+      // await audioPlayer.setSourceDeviceFile(audioPath);
+
+      // Source urlSource = UrlSource(audioPath);
+      // print("audio play here 1 url source- $urlSource");
+      // await audioPlayer.setUrl(           // Load a URL
+      //     'https://foo.com/bar.mp3');
+      print('Surya audio path - $audioPath');
+      // await audioPlayer.setAudioSource(AudioSource.uri(Uri.parse('asset:$audioPath')));
+      if (UniversalPlatform.isWeb){
+        await audioPlayer.setAudioSource(AudioSource.uri(Uri.parse(audioPath)));
+        await audioPlayer.play();
+      } else {
+        await audioPlayer.setAudioSource(AudioSource.file(audioPath));
+        await audioPlayer.play();
+      }
       // Add an event listener to be notified when the audio playback completes
-      audioPlayer.onPlayerStateChanged.listen((PlayerState state) {
-        if (state == PlayerState.completed) {
-          playing = false;
+      // audioPlayer.onPlayerStateChanged.listen((PlayerState state) {
+      //   if (state == PlayerState.completed) {
+      //     playing = false;
+      //     print("AUDIO PLAYING ENDED+++++++++++++++++++++++++++++++++++++++++++++++++");
+      //     setState(() {});
+      //   }
+      // });
 
-
-          print("AUDIO PLAYING ENDED+++++++++++++++++++++++++++++++++++++++++++++++++");
-          setState(() {});
-        }
-      });
-
-    } catch (e) {
+    } catch (e,stack) {
       print("AUDIO PLAYING++++++++++++++++++++++++${e}+++++++++++++++++++++++++");
+      print(stack);
     }
   }
 
@@ -112,6 +186,7 @@ class _RecordingScreenState extends State<RecordingScreen> {
       final url = Uri.parse('YOUR_UPLOAD_URL'); // Replace with your server's upload URL
 
       final file = File(audioPath);
+      print(' Surya file check - $file');
       if (!file.existsSync()) {
         print("UPLOADING FILE NOT EXIST+++++++++++++++++++++++++++++++++++++++++++++++++");
         return;
@@ -127,24 +202,24 @@ class _RecordingScreenState extends State<RecordingScreen> {
           ),
         );
 
-      final response = await http.Response.fromStream(await request.send());
+      // final response = await http.Response.fromStream(await request.send());
 
-      if (response.statusCode == 200) {
-        // Upload successful, you can delete the recording if needed
-        // Show a snackbar or any other UI feedback for a successful upload
-        const snackBar = SnackBar(
-          content: Text('Audio uploaded.'),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-        // Refresh the UI
-        setState(() {
-          audioPath = "";
-        });
-      } else {
-        // Handle the error or show an error message
-        print('Failed to upload audio. Status code: ${response.statusCode}');
-      }
+      // if (response.statusCode == 200) {
+      //   // Upload successful, you can delete the recording if needed
+      //   // Show a snackbar or any other UI feedback for a successful upload
+      //   const snackBar = SnackBar(
+      //     content: Text('Audio uploaded.'),
+      //   );
+      //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      //
+      //   // Refresh the UI
+      //   setState(() {
+      //     audioPath = "";
+      //   });
+      // } else {
+      //   // Handle the error or show an error message
+      //   print('Failed to upload audio. Status code: ${response.statusCode}');
+      // }
     } catch (e) {
       print('Error uploading audio: $e');
     }
@@ -153,15 +228,24 @@ class _RecordingScreenState extends State<RecordingScreen> {
   Future<void> deleteRecording() async {
 
     if (audioPath.isNotEmpty) {
+      Future<void> _deleteDir({required String dirName}) async {
+        final extDir = await getApplicationDocumentsDirectory();
+        final directoryPath = p.join(extDir.path, dirName);
+        if (await Directory(directoryPath).exists()) {
+          await Directory(directoryPath).delete(recursive: true);
+        }
+      }
       try {
         recoding_now=true;
-        File file = File(audioPath);
-        if (file.existsSync()) {
-          file.deleteSync();
-          const snackBar = SnackBar(
-            content: Text('Recoding deleted'),);
-          print("FILE DELETED+++++++++++++++++++++++++++++++++++++++++++++++++");
-        }
+        _deleteDir(dirName: 'smart_sell_pdf');
+        print("FILE DELETED+++++++++++++++++++++++++++++++++++++++++++++++++");
+        // File file = File(audioPath);
+        // if (file.existsSync()) {
+        //   file.deleteSync();
+        //   const snackBar = SnackBar(
+        //     content: Text('Recoding deleted'),);
+        //   print("FILE DELETED+++++++++++++++++++++++++++++++++++++++++++++++++");
+        // }
       } catch (e) {
         print("FILE NOT DELETED++++++++++++++++${e}+++++++++++++++++++++++++++++++++");
       }
@@ -177,15 +261,6 @@ class _RecordingScreenState extends State<RecordingScreen> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
@@ -235,3 +310,28 @@ class _RecordingScreenState extends State<RecordingScreen> {
 
   bool recoding_now=true;
 }
+
+
+
+
+// class WebBufferAudioSource extends StreamAudioSource {
+//   final Uint8List _buffer;
+//
+//   WebBufferAudioSource(this._buffer) : super(tag: "web-buffer-audio-source");
+//
+//   @override
+//   Future<StreamAudioResponse> request([int? start, int? end]) {
+//     final int lStart = start ?? 0;
+//     final int lEnd = end ?? _buffer.length;
+//
+//     return Future.value(
+//       StreamAudioResponse(
+//         sourceLength: _buffer.length,
+//         contentLength: lEnd - lStart,
+//         offset: start,
+//         contentType: 'audio/mp3',
+//         stream: Stream.value(List<int>.from(_buffer.skip(lStart).take(lEnd - lStart))),
+//       ),
+//     );
+//   }
+// }
